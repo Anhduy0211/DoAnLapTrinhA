e4b10cbdfd1b83cfb9311877085d2a2245a48830
Duy <anhduy.nguyen0211@gmail.com>
2020-11-28 19:42:39 +0700

    HelpUserScreen

---
+++ screens/HelpUser.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Text,View} from 'react-native';
+class HelpUser extends Component{
+    render() {
+        return (
+            <Text>Help User Screen</Text>
+        )
+    }
+}
+export default HelpUser;
